53e7ce149744477d3225345d3b7e180ab90f7871
davidhill94 <davidhill94@gmail.com>
2026-01-20 08:00:07 +1100

    Update /api/register route to use NextRequest for Vercel compatibility

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,9 +1,11 @@
+export const dynamic = 'force-dynamic'; // Forces render on request & NOT BUILD
+
 import bcrypt from "bcrypt";
 import prisma from "@/libs/prismadb";
-import { NextResponse  } from "next/server";
+import { NextRequest, NextResponse  } from "next/server";
 
 //API route for creating a user
-export async function POST(request: Request){
+export async function POST(request: NextRequest){
     const body = await request.json()
     const { name, email, password } = body
     const hashedPassword = await bcrypt.hash(password, 10)
